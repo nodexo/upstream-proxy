a5e29a6bb56f75504f5e33a8150732da75e7f7e5
Michael <mk@nodexo.de>
2016-08-07 16:23:06 +0200

    add skeleton code

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 const net = require('net');
 
-function UpstreamProxy() {
-  console.log('OK');
+function UpstreamProxy(routes) {
+  console.log('Server Successfully Created');
+  return net.createServer( (socket) => this.handleConnection(socket, routes) );
+}
+
+UpstreamProxy.prototype.handleConnection = (socket, routes) => {
+    //to come... :)
 }
 
 module.exports = UpstreamProxy;
